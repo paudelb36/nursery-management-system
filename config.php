4baada439875fcf50a45fb5711656c78eb2fc79d
Bibek Paudel <paudelb11@gmail.com>
2023-07-01 12:27:33 +0545

    updated by me!

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 
 
-<!-- <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "nursery_db";
 
 // Create connection
 // $conn = new mysqli($servername, $username, $password,$dbname);
