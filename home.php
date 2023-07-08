082ddddb06e7bedf2ae2b14c4e214f6683a9231f
Bibek Paudel <paudelb11@gmail.com>
2023-07-08 23:29:21 +0545

    updated.

--- home.php
+++ home.php
@@ -36,60 +36,22 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 
     <!-- Main Content Section starts -->
-    <section class="home">
-
-        <div class="content">
-            <h3>new collections</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime reiciendis, modi placeat sit cumque molestiae.</p>
-            <a href="about.php" class="btn">discover more</a>
+    <section class="banner">
+        
+        <div class="banner-content">
+            <h1>Welcome to our Plant Nursery!</h1>
+            <p>Find a wide variety of beautiful plants for your garden</p>
+            <a href="shop.php" class="btn">Shop Now</a>
         </div>
-
     </section>
 
-    <section class="products">
 
-        <h1 class="title">latest products</h1>
-
-        <div class="box-container">
-
-           //<?php
-           // $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
-            //if (mysqli_num_rows($select_products) > 0) {
-           //     while ($fetch_products = mysqli_fetch_assoc($select_products)) {
-            //?>
-                    <form action="" method="POST" class="box">
-                        <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-                        <div class="price"></div>
-                        <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-                        <div class="name"></div>
-                        <input type="number" name="product_quantity" value="1" min="0" class="qty">
-                        <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-                        <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-                        <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-                        <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-                        <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-                        <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-                    </form>
-            //<?php
-                //}
-           // } else {
-            //    echo '<p class="empty">no products added yet!</p>';
-           // }
-          //  ?>
-
-        </div>
-
-        <div class="more-btn">
-            <a href="shop.php" class="option-btn">load more</a>
-        </div>
-
-    </section>
 
     <section class="home-contact">
 
         <div class="content">
             <h3>have any questions?</h3>
-            
+
             <a href="contact.php" class="btn">contact us</a>
         </div>
 
--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ let searchButton = document.querySelector('#search-btn');
 searchButton.addEventListener('click', function() {
     searchForm.classList.toggle('active');
 });
+
+// Initialize Nivo Slider
+$(window).on('load', function() {
+    $('#slider').nivoSlider();
+});
